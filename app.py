ebe1dbb204a0d07fb6d2d3559b20c8ac3c1a718c
BrePorsch <breanneporschwilken@icloud.com>
2026-03-10 08:11:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class WilkenKeyEngine:
         }
         
          #100% PRECISION Mapping to Yale Archive IDs (MS 408)
-         Verified against Yale Catalog 2002046
+         #Verified against Yale Catalog 2002046
         self.image_map = {
              Image 3: #Intro / Erased signature
             "f1r": "1006139", 
